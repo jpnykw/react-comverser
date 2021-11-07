ea64fb991debb98aca3d5510634f31b92a140111
jpnykw <jpnykw@gmail.com>
2021-11-07 19:01:33 +0900

    :sparkles: Implement conversion for ExpressionStatement

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 const acorn = require('acorn');
 const jsx = require('acorn-jsx');
 const escodegen = require('escodegen');
+const prettier = require('prettier');
+
+const prettierOption = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  parser: 'babel',
+};
 
 const get_ast_from_code = (code: string, option = { ecmaVersion: 2017, allowImportExportEverywhere: true }) => {
   const parser = acorn.Parser.extend(jsx());
@@ -23,10 +31,13 @@ const analyze = (rawCode: string, body: any) => {
     return code;
   }
 
-  // console.log(body);
-
   for (const token of Array.isArray(body) ? body : [body]) {
     switch (token.type) {
+      case 'ImportDeclaration':
+        // console.log(token);
+        code += `${escodegen.generate(token)}\n`;
+        break;
+
       case 'ClassDeclaration':
         code += `const ${token.id.name} = (props) => {\n`;
         const classBody = token.body;
@@ -34,6 +45,10 @@ const analyze = (rawCode: string, body: any) => {
         if (classBody.type === 'ClassBody') {
           code += analyze(rawCode, classBody.body);
         }
+        break;
+
+      case 'ExportDefaultDeclaration':
+        code += `export default ${token.declaration.name}`;
         break;
 
       case 'MethodDefinition':
@@ -44,7 +59,8 @@ const analyze = (rawCode: string, body: any) => {
           // Class 内部で定義されてるメソッドを FC 用に変換
           for (const token of Array.isArray(blockStatement.body) ? blockStatement.body : [blockStatement.body]) {
             if (token?.expression?.callee?.type === 'Super') continue; // super() を無視
-            console.log(token);
+            code += analyze(rawCode, token);
+            // console.log(token);
           }
 
           // console.log('params', params, 'body', blockStatement);
@@ -61,8 +77,25 @@ const analyze = (rawCode: string, body: any) => {
         }
         break;
 
-        case 'ExportDefaultDeclaration':
-          code += `export default ${token.declaration.name}`;
+        case 'ExpressionStatement':
+          const operator = token.expression.operator;
+          const left = token.expression.left;
+          const right = token.expression.right;
+
+          if (operator === '=') {
+            // this プロパティに対する代入
+            if (left.type === 'MemberExpression' && left.object.type === 'ThisExpression') {
+              if (left.property.name === 'state') {
+                // state を hooks に置き換える
+                for (const {key, value} of right.properties) {
+                  const name = key.name;
+                  code += `const [${name}, set${name[0].toUpperCase()}${name.slice(1, name.length)}] = React.useState(${value.raw})\n`;
+                }
+              } else {
+                code += `const ${left.property.name} = ${escodegen.generate(right)}\n`;
+              }
+            }
+          }
           break;
     }
   }
@@ -89,5 +122,11 @@ const code = `
   export default Component;
 `;
 
+console.log('\nInput:\n');
+console.log('\x1b[36m%s\x1b[0m', prettier.format(code, prettierOption));
+
 const ast = get_ast_from_code(code);
-console.log(analyze(code, ast.body));
+const result = prettier.format(analyze(code, ast.body), prettierOption);
+
+console.log('Output:\n');
+console.log('\x1b[35m%s\x1b[0m', result);
