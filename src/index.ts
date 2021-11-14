9b3087a99548e31ba58afbdccbb275368e7eee44
jpnykw <jpnykw@gmail.com>
2021-11-15 00:00:48 +0900

    :sparkles: Implement command line arguments for reading files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import prettier from 'prettier'
+import argv from 'minimist'
+import fs from 'fs'
 import get_ast_from_code from './ast'
 import convert_cc_to_fc from './convert'
 
@@ -9,37 +11,20 @@ const prettierOption = {
   parser: 'babel',
 }
 
-const code = `
-  import React from 'react';
-  class Component extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        count: 123,
-      };
-    }
-    countUp() {
-      this.setState({
-        count: this.state.count + 1,
-      });
-    }
-    countDown() {
-      this.setState({
-        count: this.state.count - 1,
-      });
-    }
-    render() {
-      return (
-        <>
-          <div>current count: {this.state.count}</div>
-          <button onClick={() => this.countUp()}>count up</button>
-          <button onClick={() => this.countDown()}>count down</button>
-        </>
-      );
-    }
-  }
-  export default Component;
-`
+const { path } = argv(process.argv.slice(2))
+
+if (path === undefined || path === true) {
+  console.log('\x1b[31mplease set the file path using argument:\x1b[0m', '--path=PATH')
+  process.exit()
+}
+
+if (!fs.existsSync(path)) {
+  console.log('\x1b[31mno such file:\x1b[0m', path)
+  process.exit()
+}
+
+const file = fs.readFileSync(path)
+const code = file.toString()
 
 console.log('\nInput:\n')
 console.log('\x1b[36m%s\x1b[0m', prettier.format(code, prettierOption))
